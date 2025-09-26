c8e7a2c0e8d6b370d85f0d83c02b6983bfaa6920
christian <kadiuchristian8@gmail.com>
2025-09-27 00:26:12 +0200

    minor changes

--- pacman/pacman.c
+++ pacman/pacman.c
@@ -1,6 +1,54 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+typedef struct pacman{
+    int x;
+    int y;
+} pacman;
+
+typedef enum dir{
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+}dir;
+
+pacman* init(){
+    pacman* pac = (pacman*)malloc(sizeof(pacman));
+    pac->x = 21;
+    pac->y = 22;
+    return pac;
+}
+
+dir getDirection(){
+    char d;
+    dir di;
+    scanf("%c", &d);
+    if (d == 'w'){
+        di = UP;
+    }else if(d == 's'){
+        di = DOWN;
+    }else if(d == 'a'){
+        di = LEFT;
+    }else{
+        di = RIGHT;
+    }
+    return di;
+}
+
+void updatePosition(pacman* p, dir d){
+    switch(d){
+        case 0: p->y -= 1; break;
+        case 1: p->y += 1; break;
+        case 2: p->x -= 1; break;
+        case 3: p->x += 1; break;
+    }
+}
 
 int main(){
+    pacman* pac = init();
+    dir direction = LEFT;
     FILE* fp = fopen("mappa.txt", "r");
 
     if(fp == NULL){
@@ -30,5 +78,11 @@ int main(){
             printf("%c", mappa[i][k]);
         }
         printf("\n");
+    }
+
+    while(1){
+        direction = getDirection();
+        updatePosition(pac, direction);
+        sleep(1000);
     }
 }
